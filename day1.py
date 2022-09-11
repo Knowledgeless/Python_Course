b3c7e3962ee6d8391e35414a32c8a2d8d370608e
Knowledgeless <nazmulhossain914@gmail.com>
2022-09-11 20:43:59 +0600

    Uploading Day1

---
+++ day1.py
@@ -0,0 +1,35 @@
+# a = "Nazmul"
+# b = 12
+# c = 3.1415
+
+# print(type(a), type(b), type(c))
+
+# a = "Nazmul"
+# b = "Hossain"
+
+# print(a+b)
+
+from math import ceil, floor
+from re import sub
+
+
+val1 = 20
+val2 = 3
+
+sumation = val1 + val2
+subtraction = val1 - val2
+multiplication = val1 * val2
+division = val1 / val2
+
+expo = val1 ** val2
+floor_div = val1 // val2
+
+# print('Sumation of ', val1, "and", val2, " is ", sumation)
+# print("Sumation of {} and {} is {}".format(val1, val2, sumation))
+
+print(f"Sumation of {val1} and {val2} is {sumation}")
+print(f"Subtraction of {val1} and {val2} is {subtraction}")
+print(f"Multiplication of {val1} and {val2} is {multiplication}")
+print(f"Division of {val1} and {val2} is {division}")
+print(f"Exponent value of {val1} and {val2} is {expo}")
+print(f"Floor Div of {val1} and {val2} is {floor_div}")
